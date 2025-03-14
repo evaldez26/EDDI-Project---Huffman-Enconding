2092abfce3136e1e57f62dbdb07b3e62b9c3eadc
aritabryan <147879047+aritabryan@users.noreply.github.com>
2025-03-13 19:25:51 -0600

    Update EDDI Project - Huffman Enconding.cpp
    cambio 1 Bryan

--- EDDI Project - Huffman Enconding.cpp
+++ EDDI Project - Huffman Enconding.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include <vector>
+#include <queue>
+#include "Huffman.h"
 using namespace std;
-unordered_map<char, int> calcularFrecuencia(const string texto) {
-    unordered_map<char, int>frecuencias;//unordered_map es un hashmap que no ordena las claves
-
-    for (char c : texto) {//Recorre el texo y entra cada vez que encuentre el mismo caracter
-        frecuencias[c]++;
-    }
-    return frecuencias;
- }
 int main()
 {
-    string texto;
-    cout << "Ingresa un texto: ";
-    getline(cin, texto);
-    unordered_map<char, int> frecuencias = calcularFrecuencia(texto);
-    for (const auto elemento : frecuencias) {
-        cout << elemento.first << ": " << elemento.second<<endl;
-    }
-    return 0;
+    Huffman huff;
+    string text;
+    cout << "Ingrese el texto: ";
+    getline(cin, text);
+    huff.buildHuffmanTree(text);
 }
