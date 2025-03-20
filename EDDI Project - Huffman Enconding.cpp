09e1f37b674fffc65817b2f7e99408c9db97d07c
aritabryan <147879047+aritabryan@users.noreply.github.com>
2025-03-20 15:25:41 -0600

    Update EDDI Project - Huffman Enconding.cpp

--- EDDI Project - Huffman Enconding.cpp
+++ EDDI Project - Huffman Enconding.cpp
@@ -7,38 +7,41 @@
 using namespace std;
 int main()
 {
-
-Huffman huff;
-string text;
-cout << "Ingrese el texto: ";
-getline(cin, text);
-huff.crearHuffmanTree(text);
-
     //para probar cargar un archivo de texto creen uno con la frase que quieran y lo ponen en el directorio del proyecto, y ponen el nombre del archivo en el menu
     //si prueban cargar un archivo binario sin haber corrido el programa obvio les va a decir que hubo error, tienen que correrlo primero
-
-    
+    Huffman huff;
     int opcion;
+    string nombreArchivoTexto;
+    string nombreArchivoBinario;
+    string nombreArchivo;
     cout << "Seleccione una opcion:\n";
-    cout << "1. Cargar texto para codificar.\n";
-    cout << "2. Cargar archivo binario codificado.\n";
+    cout << "1. Cargar texto para codificar\n";
+    cout << "2. Cargar archivo binario codificado\n";
     cin >> opcion;
-
-    if (opcion == 1) {
-        string nombreArchivoTexto;
-        cout << "Ingrese el nombre del archivo de texto a cargar: "; //estaba usando input.txt para el nombre del archivo, se podria cambiar y aqui poner como se llama
-        cin >> nombreArchivoTexto;
-        huff.cargarTexto(nombreArchivoTexto);
-    }
-    else if (opcion == 2) {
-        string nombreArchivoBinario;
-        cout << "Ingrese el nombre del archivo binario a cargar: "; //el programa crea el archivo binario con el nombre "huffman.bin"
-        cin >> nombreArchivoBinario;
-        huff.cargarCodificado(nombreArchivoBinario);
-    }
-    else {
-        cout << "Opción no válida.\n";
-    }
+    do {
+        switch (opcion) {
+        case 1:
+            cout << "Ingrese el nombre del archivo de texto a cargar: "; //estaba usando input.txt para el nombre del archivo, se podria cambiar y aqui poner como se llama
+            cin >> nombreArchivoTexto;
+            nombreArchivo = nombreArchivoTexto;
+            huff.cargarTexto(nombreArchivoTexto);
+            break;
+        case 2:
+            cout << "Ingrese el nombre del archivo binario a cargar: "; //el programa crea el archivo binario con el nombre "huffman.bin"
+            cin >> nombreArchivoBinario;
+            huff.cargarCodificado(nombreArchivoBinario);
+            break;
+        default:
+            cout << "Opcion no valida\n";
+			break;
+        }
+    
+        cout << "\n\nSeleccione una opcion:\n";
+        cout << "1. Cargar texto para codificar\n";
+        cout << "2. Cargar archivo binario codificado\n";
+        cout << "3. Salir\n";
+        cin >> opcion;
+    } while (opcion != 3);
 
     return 0;
 }
